291426cab54e19e94c4b38439055352bf499f520
baxter101 <nimrod.bal@gmail.com>
2019-04-05 16:43:25 +0200

    corrected 'converter' ref in main and restore banana model and texture file references

--- converter-and-renderer/main.py
+++ converter-and-renderer/main.py
@@ -4,7 +4,7 @@ import banana
 
 
 
-convert.main("data/ModelNet10/monitor/test/monitor_0466.off","data/test.obj","obj",None,None)
+converter.main("data/ModelNet10/monitor/test/monitor_0466.off","data/test.obj","obj",None,None)
 
 
 # banana.peel(10,50) # (Number of viewpoints, Distance from model)
