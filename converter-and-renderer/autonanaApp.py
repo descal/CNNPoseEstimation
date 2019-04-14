1e999c520b61c9a961a0860fda4a278af6f0bf65
baxter101 <nimrod.bal@gmail.com>
2019-04-14 18:01:16 +0200

    added YOLO3 annontation file support and folder crawler for batch executions

--- converter-and-renderer/autonanaApp.py
+++ converter-and-renderer/autonanaApp.py
@@ -4,11 +4,12 @@ import os
 import numpy as np
 from PIL import Image, ImageOps, ImageMath, ImageDraw
 from application import Application
-
+from pathlib import Path
+import os
 def local(*path):
     return os.path.normpath(os.path.join(os.path.dirname(__file__), *path))
 
-class BananaApp(Application):
+class AutonanaApp(Application):
     RESOLUTION = (640,480)
 
     # model           = 'data/Banana.obj'    # relative path to object file
@@ -22,15 +23,19 @@ class BananaApp(Application):
     orientation = 0.0, 00.0, 0.0        # model orientation (degrees)
 
 
-    light       = 10.0, 10.0, 10.0     # light source position
+    light       = 100.0, 100.0, 100.0     # light source position
 
     fov         = 58.0                 # camera field of view in degrees
-    cam         = 1.5, 0.0, 0.0        # camera position
+    cam         = 0.5, 0.0, 0.0        # camera position
     target      = 0.0 , 0.0, 0.0       # camera target position
     roll        = 0.0                  # camera roll in degrees
 
-    def __init__(self):
-        super(BananaApp, self).__init__()
+    def __init__(self,mod,tex=None):
+        super(AutonanaApp, self).__init__()
+
+        self.model = mod
+        self.texture = tex
+
         self._index = 0
         self.load_model(self.model, self.texture)   # load model with texture
         # self.load_model(self.model)               # load model
@@ -41,7 +46,7 @@ class BananaApp(Application):
         self.disable_color()                  # disable color (overlay)
 
         self.set_light_position(self.light)    # set and enable light
-        self.disable_light()                  # disable light
+        # self.disable_light()                  # disable light
 
         self.set_model_from_euler(self.position, self.orientation)    # position object in scene
         self.set_view_from_target(self.cam, self.target, self.roll)   # position camera in scene
@@ -56,12 +61,18 @@ class BananaApp(Application):
 
 
     def save_to_file(self, images, folder='output'):
-
+        # # path = Path(self.model)
+        # print(Path(self.model).filename)
+        model_name = os.path.basename(Path(self.model))
+        model_name = model_name[:len(model_name)-4]
+        folder = 'output/'+str(model_name)
         rgb_file       = local(folder, 'rgb_{:04}.png'.format(self._index))
         depth_file     = local(folder, 'depth_{:04}.png'.format(self._index))
         mask_file      = local(folder, 'mask_{:04}.png'.format(self._index))
         annotated_file = local(folder, 'annotated_{:04}.png'.format(self._index))
         roi_file       = local(folder, 'roi_{:04}.txt'.format(self._index))
+        yolo_file       = local(folder, 'yolo_{:04}.txt'.format(self._index))
+
         cam_pose_file  = local(folder, 'camera_{:04}.txt'.format(self._index))
         obj_pose_file  = local(folder, 'model_{:04}.txt'.format(self._index))
         self._index += 1
@@ -73,11 +84,25 @@ class BananaApp(Application):
         images['MASK'].save(mask_file)
 
         roi = images['MASK'].getbbox()
+        left, upper, right, lower = images['MASK'].getbbox()
         if roi is None: roi = (0,0,0,0)
         annotated = images['RGB'].copy()
         draw = ImageDraw.Draw(annotated)
         draw.rectangle(roi, outline=(255,0,0))
         annotated.save(annotated_file)
+        w = (right-left) #bbox width in pixles
+        h = (lower-upper) #bbox height in pixles
+        cx = float((left + w/2)/self.RESOLUTION[0]) #bbox center x
+        cy = float((upper + h/2)/self.RESOLUTION[1])
+
+        w = float(w/self.RESOLUTION[0]) #Yolo3 standard requires that values are relative to image size
+        h = float(h/self.RESOLUTION[1])  #Yolo3 standard requires that values are relative to image size
+
+        yolo3std = model_name+" "+"{:.6f}".format(cx)+" "+"{:.6f}".format(cy)+" "+"{:.6f}".format(w)+" "+"{:.6f}".format(h)
+
+        #Write yolo annotation file
+        yolo = open(yolo_file,'w')
+        yolo.write(yolo3std)
 
         with open(roi_file,'wb') as f:
             np.savetxt(f, [roi], fmt='%d', delimiter=' ')
